53591e33e836136dc59878dca85c3a4e385f97c3
alvaroluiss <alvaro.luiscosta@hotmail.com>
2024-06-29 12:17:43 -0300

    Create test to RBAC

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,8 +1,19 @@
 import { app } from '@/app'
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
+export async function createAndAuthenticateUser(app: FastifyInstance, isAdmim?: boolean) {
+    const user = await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password_hash: await hash('123456', 6),
+            role: isAdmim ? 'ADMIN' : 'MEMBER',
+        }
+    })
+
     await request(app.server)
             .post('/users')
             .send({
